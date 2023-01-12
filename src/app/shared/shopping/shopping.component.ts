7e63b1a5d062ca9bc1e018a57fbd6ed9a5b2ab81
#lagonzalezdev <gonzalezortizlau@gmail.com>
2023-01-12 18:53:24 -0500

    shopping cart

--- src/app/shared/shopping/shopping.component.ts
+++ src/app/shared/shopping/shopping.component.ts
@@ -8,3 +8,24 @@ import { Component } from '@angular/core';
 export class ShoppingComponent {
 
 }
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+
+if(document.readyState == 'loading'){
+  document.addEventListener("DOMContentLoaded", ready);
+} else{
+  ready();
+}
+
+function ready(){
+  var removeCartButtons = document.getElementsByClassName('cart-remove')
+  console.log(removeCartButtons)
+  for (var i = 0; i < removeCartButtons.length; i++){
+    var button = removeCartButtons [i]
+
+  }
+}
+
+
